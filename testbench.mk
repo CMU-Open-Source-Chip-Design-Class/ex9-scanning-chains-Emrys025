c39014666343525f3526272c1faafee1f7cc2de2
Emrys025 <c3gu@ucsd.edu>
2025-03-30 10:08:41 -0400

    fsm test

--- testbench.mk
+++ testbench.mk
@@ -1,6 +1,6 @@
 TOPLEVEL_LANG = verilog
-VERILOG_SOURCES = $(shell pwd)/adder/adder_out.sv
-TOPLEVEL = adder
+VERILOG_SOURCES = $(shell pwd)/hidden_fsm/hidden_fsm.sv
+TOPLEVEL = hidden_fsm
 MODULE = ScanChain_starter
 SIM = verilator
 EXTRA_ARGS += --trace -Wno-WIDTHTRUNC -Wno-UNOPTFLAT -Wno-fatal
